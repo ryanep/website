e35ea3cfa0c122f7fa00ed71b6cea29044aa3738
Ryan Elliott-Potter <ryan@ryanep.com>
2017-03-04 17:25:38 +0000

    Add and implement react helmet

--- src/app/containers/home/index.js
+++ src/app/containers/home/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Helmet from 'react-helmet';
 import styles from './style.scss';
 
 export default class Home extends Component {
@@ -6,6 +7,7 @@ export default class Home extends Component {
     render() {
         return (
         	<main className={styles.home}>
+        		<Helmet title={'Home - Ryan Elliott-Potter'} />
         		Home
         	</main>
         )
