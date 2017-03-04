b944af4984f0adfad57f657a4d2e3a26751b2617
Ryan Elliott-Potter <ryan@ryanep.com>
2017-03-04 17:24:49 +0000

    Add styling to header component

--- src/app/components/header/index.js
+++ src/app/components/header/index.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import { Link, IndexLink } from 'react-router';
+import Navigation from '../navigation';
 import styles from './style.scss';
 
 export default class Header extends Component {
 
     render() {
+    	let className = this.props.path === '/' ? styles.banner : styles.header;
+    	
         return (
-            <header className={styles.header}>
-            	Header
+            <header className={className}>
+            	<Navigation />
+            	<div className={styles.heading}>
+	            	<h1>Ryan Elliott-Potter</h1>
+	            	<p>Web Developer</p>
+            	</div>
             </header>
         )
     }
