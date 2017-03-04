daad9269d8047f245a2b2a80332be4a11729d4e4
Ryan Elliott-Potter <ryan@ryanep.com>
2017-03-04 17:25:49 +0000

    Add route for about page

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from '../components/app';
 import Home from '../containers/home';
+import About from '../containers/about';
 
 export default (
 	<Route path="/" component={App}>
 		<IndexRoute component={Home} />
+		<Route path="about" component={About} />
 	</Route>
 );
