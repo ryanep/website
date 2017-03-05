59849198e76dffeb47226973b982690760ba7a94
Ryan Elliott-Potter <ryan@ryanep.com>
2017-03-05 16:22:29 +0000

    Add title bar to about page and minor CSS changes

--- src/app/containers/about/index.js
+++ src/app/containers/about/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Helmet from 'react-helmet';
+import TitleBar from '../../components/title-bar';
 import styles from './style.scss';
 
 export default class About extends Component {
@@ -8,6 +9,7 @@ export default class About extends Component {
         return (
         	<main className={styles.about}>
         		<Helmet title={'About - Ryan Elliott-Potter'} />
+        		<TitleBar heading={'About Me'} />
         		About
         	</main>
         )
