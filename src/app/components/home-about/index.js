96a64418db1d2040c505e9ad27e5a99611175fa3
Ryan Elliott-Potter <ryan@ryanep.com>
2017-03-07 20:43:16 +0000

    Mobile first styling changes

--- src/app/components/home-about/index.js
+++ src/app/components/home-about/index.js
@@ -9,7 +9,7 @@ export default class HomeAbout extends Component {
             <section className={styles.about}>
             	<div className={styles.wrap}>
             		<img src={this.props.image} width="120" height="120" className={styles.photo} alt="Ryan Elliott-Potter" />
-            		<h1 className={styles.heading}>{this.props.title}</h1>
+            		<h2 className={styles.heading}>{this.props.title}</h2>
             		<p className={styles.info}>{this.props.desc}</p>
             		<Link to={'/about'} className={styles.button}>{this.props.cta}</Link>
             	</div>
