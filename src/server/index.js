bd929fc21190fa36fd41aeca631df0daa682c08d
Ryan Elliott-Potter <ryan@ryanep.com>
2017-03-07 21:39:22 +0000

    Add compression module

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { match } from 'react-router';
+import compression from 'compression';
 import config from './config';
 import routes from '../app/routes';
 import { handleRender, handleNotFound } from './render';
@@ -8,6 +9,8 @@ const app = express();
 
 app.set('views', process.cwd() + '/src/server/views');
 app.set('view engine', 'ejs');
+
+app.use(compression());
 
 app.use(express.static('dist/public'));
 
