becfcc19c2892e1c0516251d9a2abce0609e2924
Ryan Elliott-Potter <ryan@ryanep.com>
2017-03-07 20:43:06 +0000

    Add new components to home container

--- src/app/containers/home/index.js
+++ src/app/containers/home/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Helmet from 'react-helmet';
 import HomeAbout from '../../components/home-about';
+import HomeExperience from '../../components/home-experience';
+import HomeTimeline from '../../components/home-timeline';
 import styles from './style.scss';
 
 export default class Home extends Component {
@@ -10,6 +12,8 @@ export default class Home extends Component {
         	<main className={styles.main}>
         		<Helmet title={'Home - Ryan Elliott-Potter'} />
         		<HomeAbout image={'https://ryanep.com/images/me@2x.jpg'} title={'About Me'} desc={'My name is Ryan Elliott-Potter and I am a software engineer and web developer. I have a passion for all types of technology and enjoy developing software and web applications.'} cta={'Learn more'}  />
+        		<HomeExperience />
+        		<HomeTimeline />
         	</main>
         )
     }
