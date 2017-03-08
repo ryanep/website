a53b32f3d82d66ac9d4dcf3137fc275bad826b1d
Ryan Elliott-Potter <ryan@ryanep.com>
2017-03-08 20:13:43 +0000

    Created experience icon component

---
+++ src/app/components/home-experience/experience-icon/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import styles from './style.scss';
+
+export default class ExperienceIcon extends Component {
+
+    render() {
+
+    	var style = {
+    		backgroundColor: this.props.data.colour
+    	}
+
+        return (
+            <li className={styles.icon} style={style} title={this.props.data.name}>
+            	{this.props.data.name}
+            </li>
+        )
+    }
+    
+}
--- src/app/components/home-experience/index.js
+++ src/app/components/home-experience/index.js
@@ -1,14 +1,55 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import ExperienceIcon from './experience-icon';
 import styles from './style.scss';
 
 export default class HomeExperience extends Component {
 
     render() {
+
+    	let icons = [
+    		{
+    			name: 'HTML',
+    			colour: 'blue'
+    		},
+    		{
+    			name: 'CSS',
+    			colour: 'red'
+    		},
+    		{
+    			name: 'JavaScript',
+    			colour: 'red'
+    		},
+    		{
+    			name: 'Node.js',
+    			colour: 'red'
+    		},
+    		{
+    			name: 'PHP',
+    			colour: 'red'
+    		},
+    		{
+    			name: 'WordPress',
+    			colour: 'red'
+    		},
+    		{
+    			name: 'MySQL',
+    			colour: 'red'
+    		},
+    		{
+    			name: 'MongoDB',
+    			colour: 'red'
+    		},
+    	];
+
         return (
             <section className={styles.experience}>
             	<div className={styles.wrap}>
-            		<h2 className={styles.heading}>Experience</h2>
+            		<h2 className={styles.heading}>Technology</h2>
+            		<p className={styles.desc}>I have experience is a number of different technologies, some of which are listed below:</p>
+            		<ul className={styles.icons}>
+            			{icons.map((icon, index) => <ExperienceIcon key={index} data={icon} />)}
+            		</ul>
             	</div>
             </section>
         )
