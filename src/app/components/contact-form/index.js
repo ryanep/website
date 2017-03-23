85340319f93d11ab88e706542f5519fa094401c0
Ryan Elliott-Potter <ryan@ryanep.com>
2017-03-23 21:34:56 +0000

    Improve styling of contact form

--- src/app/components/contact-form/index.js
+++ src/app/components/contact-form/index.js
@@ -16,17 +16,19 @@ export default class ContactForm extends Component {
 			    	<p className={styles.desc}>Feel free to contact me</p>
 
 					<form className={styles.form} onSubmit={this.handleSubmit}>
-						<div className={styles.section}>
-							<label className={styles.label} htmlFor={'name'}>Name</label>
-							<input className={styles.textbox} type="text" id="name" name="name" required />
-							<label className={styles.label} htmlFor={'subject'}>Subject</label>
-							<input className={styles.textbox} type="text" id="subject" name="subject" required />
-							<label className={styles.label} htmlFor={'email'}>Email</label>
-							<input className={styles.email} type="email" id="email" name="email" required />
-						</div>
-						<div className={styles.section}>
-							<label className={styles.label} htmlFor={'message'}>Message</label>
-							<textarea className={styles.textarea} id="message" name="message" required></textarea>
+						<div className={styles.sections}>
+							<div className={styles.left}>
+								<label className={styles.label} htmlFor={'name'}>Name</label>
+								<input className={styles.textbox} type="text" id="name" name="name" required />
+								<label className={styles.label} htmlFor={'subject'}>Subject</label>
+								<input className={styles.textbox} type="text" id="subject" name="subject" required />
+								<label className={styles.label} htmlFor={'email'}>Email</label>
+								<input className={styles.email} type="email" id="email" name="email" required />
+							</div>
+							<div className={styles.right}>
+								<label className={styles.label} htmlFor={'message'}>Message</label>
+								<textarea className={styles.textarea} id="message" name="message" required></textarea>
+							</div>
 						</div>
 						<input type="submit" value="Send message" className={styles.button} />
 					</form>
