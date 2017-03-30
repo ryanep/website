de6c090be54b73922c1574037362501ff046e093
Ryan Elliott-Potter <ryan@ryanep.com>
2017-03-30 21:48:19 +0100

    Change home about photo

--- src/app/containers/home/index.js
+++ src/app/containers/home/index.js
@@ -13,7 +13,7 @@ export default class Home extends Component {
         return (
         	<main className={styles.main}>
         		<Helmet title={'Home - Ryan Elliott-Potter'} />
-        		<HomeAbout image={'https://ryanep.com/images/me@2x.jpg'} title={'About Me'} desc={'My name is Ryan Elliott-Potter and I am a software engineer and web developer. I have a passion for all types of technology and enjoy developing software and web applications.'} cta={'Learn more'}  />
+        		<HomeAbout image={'https://avatars2.githubusercontent.com/u/11217494?v=3&s=460'} title={'About Me'} desc={'My name is Ryan Elliott-Potter and I am a software engineer and web developer. I have a passion for all types of technology and enjoy developing software and web applications.'} cta={'Learn more'}  />
         		<HomeExperience />
                 <HomeWork />
         		<HomeTimeline />
