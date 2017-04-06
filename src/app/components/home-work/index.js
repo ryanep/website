268050c4ac561a83ecf119319da862b4c6b078f9
Ryan Elliott-Potter <ryan@ryanep.com>
2017-04-06 21:52:04 +0100

    Create work item component

--- src/app/components/home-work/index.js
+++ src/app/components/home-work/index.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
+import WorkItem from './work-item';
 import styles from './style.scss';
 
 export default class HomeWork extends Component {
 
     render() {
+
+        let work = [
+            {
+                image: 'https://ryanep.com/images/cake.jpg',
+                desc: 'This is an example description'
+            },
+            {
+                image: 'https://ryanep.com/images/hainault.jpg',
+                desc: 'This is an example description'
+            },
+            {
+                image: 'https://ryanep.com/images/beerhunter.jpg',
+                desc: 'This is an example description'
+            }
+        ];
+
         return (
             <section className={styles.work}>
                 <div className={styles.wrap}>
                     <h2 className={styles.heading}>My work</h2>
+                    <p className={styles.desc}>Here are some of my personal projects</p>
+                    <div className={styles.items}>
+                        {work.map((item, index) => <WorkItem key={index} image={item.image} desc={item.desc} />)}
+                    </div>
+                    <Link to={'/work'} className={styles.button}>See more work</Link>
                 </div>
             </section>
         )
---
+++ src/app/components/home-work/work-item/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import styles from './style.scss';
+
+export default class HomeWork extends Component {
+
+    render() {
+        return (
+            <div className={styles.work}>
+                <img src={this.props.image} className={styles.image} />
+                <p className={styles.desc}>{this.props.desc}</p>
+            </div>
+        )
+    }
+    
+}
