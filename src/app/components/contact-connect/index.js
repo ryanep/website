986b9d0066b5df0176428fa0df95485ee4113de5
Ryan Elliott-Potter <ryan@ryanep.com>
2017-04-06 21:51:40 +0100

    Create simple connect component

---
+++ src/app/components/contact-connect/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import styles from './style.scss';
+
+export default class ContactConnect extends Component {
+
+	render() {
+		return (
+		    <div className={styles.connect}>
+		    	<div className={styles.wrap}>
+		    		<h1 className={styles.heading}>Connect with me</h1>
+		    		<p className={styles.desc}>You can connect with me on a number of accounts</p>
+		    	</div>
+		    </div>
+		)
+	}
+	
+}
