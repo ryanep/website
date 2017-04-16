ac94d0c3310540a11c1a59e06d18ef8bae642a3b
Ryan Elliott-Potter <ryan@ryanep.com>
2017-04-16 16:38:16 +0100

    Change route file to point to correct app container component path

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import App from '../components/app';
+import App from '../containers/app';
 import Home from '../containers/home';
 import About from '../containers/about';
 import Blog from '../containers/blog';
