5197b4549d421d0ae3fed58815673cdd3b010414
Ryan Elliott-Potter <ryan@ryanep.com>
2017-04-16 16:35:25 +0100

    Decouple content on home work component

--- src/app/components/home-work/index.js
+++ src/app/components/home-work/index.js
@@ -6,31 +6,15 @@ import styles from './style.scss';
 export default class HomeWork extends Component {
 
     render() {
-
-        let work = [
-            {
-                image: 'https://ryanep.com/images/cake.jpg',
-                desc: 'This is an example description'
-            },
-            {
-                image: 'https://ryanep.com/images/hainault.jpg',
-                desc: 'This is an example description'
-            },
-            {
-                image: 'https://ryanep.com/images/beerhunter.jpg',
-                desc: 'This is an example description'
-            }
-        ];
-
         return (
             <section className={styles.work}>
                 <div className={styles.wrap}>
-                    <h2 className={styles.heading}>My work</h2>
-                    <p className={styles.desc}>Here are some of my personal projects</p>
+                    <h2 className={styles.heading}>{this.props.content.title}</h2>
+                    <p className={styles.desc}>{this.props.content.desc}</p>
                     <div className={styles.items}>
-                        {work.map((item, index) => <WorkItem key={index} image={item.image} desc={item.desc} />)}
+                        {this.props.content.work.map((item, index) => <WorkItem key={index} image={item.image} alt={item.imageAlt} desc={item.desc} />)}
                     </div>
-                    <Link to={'/work'} className={styles.button}>See more work</Link>
+                    <Link to={this.props.content.aboutButtonURL} className={styles.button}>{this.props.content.aboutButtonText}</Link>
                 </div>
             </section>
         )
