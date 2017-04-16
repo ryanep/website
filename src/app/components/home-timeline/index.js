54a344082f13a9e1dad6755d7fa8ed8f07b5b6e2
Ryan Elliott-Potter <ryan@ryanep.com>
2017-04-16 16:35:48 +0100

    Decouple content on home timeline component

--- src/app/components/home-timeline/index.js
+++ src/app/components/home-timeline/index.js
@@ -6,35 +6,13 @@ import styles from './style.scss';
 export default class HomeTimeline extends Component {
 
     render() {
-
-    	let timeline = [
-    		{
-    			company: 'MMT Digital',
-    			role: 'Frontend / PHP developer',
-    			time: '2014 - present',
-                url: '/work#mmt'
-    		},
-    		{
-    			company: 'in.house.media',
-    			role: 'Frontend / PHP developer',
-    			time: '2012 - 2014',
-                url: '/work#ihm'
-    		},
-    		{
-    			company: 'Raffenday',
-    			role: 'Frontend / PHP developer',
-    			time: '2011 - 2014',
-                url: '/work#raffenday'
-    		}
-    	];
-
         return (
             <section className={styles.timeline}>
             	<div className={styles.wrap}>
-            		<h2 className={styles.heading}>Work Experience</h2>
-            		<p className={styles.desc}>Below is a timeline of my career history.</p>
+            		<h2 className={styles.heading}>{this.props.content.title}</h2>
+            		<p className={styles.desc}>{this.props.content.desc}</p>
             		<div className={styles.items}>
-            			{timeline.map((item, index) => <TimelineItem key={index} data={item} />)}
+            			{this.props.content.timeline.map((item, index) => <TimelineItem key={index} data={item} />)}
             		</div>
             	</div>
             </section>
