9b0dbbf1dcf3de445156d6f8df934079102ccd46
Ryan Elliott-Potter <ryan@ryanep.com>
2017-05-03 21:25:55 +0100

    Remove lazy load for above-the-fold content

--- src/app/components/home-about/index.js
+++ src/app/components/home-about/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import ReactMarkdown from 'react-markdown';
-import LazyLoad from 'react-lazyload';
 import styles from './style.scss';
 
 export default class HomeAbout extends Component {
@@ -10,9 +9,7 @@ export default class HomeAbout extends Component {
         return (
             <section className={styles.about}>
             	<div className={styles.wrap}>
-                    <LazyLoad width={120} height={120}>
-            		  <img src={this.props.content.image} width="120" height="120" className={styles.photo} alt={this.props.content.imageAlt} />
-                    </LazyLoad>
+                    <img src={this.props.content.image} width="120" height="120" className={styles.photo} alt={this.props.content.imageAlt} />
             		<h2 className={styles.heading}>{this.props.content.title}</h2>
                     <ReactMarkdown className={styles.info} source={this.props.content.desc} />
             		<Link to={this.props.content.buttonURL} className={styles.button}>{this.props.content.buttonText}</Link>
