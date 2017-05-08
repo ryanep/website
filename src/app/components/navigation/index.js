5e6a2e1e08ef242157c10fc2a869dc4ea46849cc
Ryan Elliott-Potter <ryan@ryanep.com>
2017-05-08 20:11:14 +0100

    Implement mobile navigation

--- src/app/components/navigation/index.js
+++ src/app/components/navigation/index.js
@@ -4,8 +4,15 @@ import styles from './style.scss';
 
 export default class Navigation extends Component {
 
-    burgerButtonClicked() {
-        console.log('Burger clicked');
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false
+        }
+    }
+
+    burgerButtonClicked = () => {
+        this.setState({ open: !this.state.open });
     }
 
     render() {
@@ -15,14 +22,14 @@ export default class Navigation extends Component {
                     <IndexLink to={'/'} className={styles.logolink}>
                         <img src="/images/logo.svg" width="80" height="30" alt="Logo" className={styles.logo} />
                     </IndexLink>
-                    <div className={styles.links}>
+                    <div className={this.state.open ? styles.linksOpen : styles.links}>
                     	<IndexLink to={'/'} className={styles.link} activeClassName={styles.active}>Home</IndexLink>
                     	<Link to={'/about'} className={styles.link} activeClassName={styles.active}>About</Link>
                     	<Link to={'/work'} className={styles.link} activeClassName={styles.active}>Work</Link>
                         <Link to={'/blog'} className={styles.link} activeClassName={styles.active}>Blog</Link>
                     	<Link to={'/contact'} className={styles.link} activeClassName={styles.active}>Contact</Link>
                     </div>
-                    <div className={styles.burger} onClick={this.burgerButtonClicked}>
+                    <div className={this.state.open ? styles.burgerOpen : styles.burger} onClick={this.burgerButtonClicked}>
                         <div className={styles.hide}>Menu</div>
                     </div>
                 </div>
