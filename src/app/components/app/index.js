d72fcacb47a4f40a711cfa8414435ed39602812e
Ryan Elliott-Potter <ryan@ryanep.com>
2017-07-03 21:06:48 +0100

    Move app component from container folder to components

---
+++ src/app/components/app/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import styles from './style.scss';
+import Header from '../../components/header';
+import Footer from '../../components/footer';
+import Loader from '../../components/loader';
+import Home from '../../containers/home';
+import About from '../../containers/about';
+import Work from '../../containers/work';
+import Contact from '../../containers/contact';
+import Error from '../../containers/error';
+
+export default class App extends Component {
+	render() {
+		return (
+			<div className={styles.app}>
+				<Header path={this.props.location.pathname} />
+				<Switch>
+					<Route exact path={'/'} component={Home} />
+					<Route exact path={'/about'} component={About} />
+					<Route exact path={'/work'} component={Work} />
+					<Route exact path={'/contact'} component={Contact} />
+					<Route component={Error} />
+				</Switch>
+				<Footer />
+				{/*<Loader isLoading={this.props.isLoading} />*/}
+			</div>
+		);
+	}
+}
