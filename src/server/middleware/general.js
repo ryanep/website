da8432f4be7202a919b9b02fe1ec6d67acaefcf6
Ryan Elliott-Potter <ryan@ryanep.com>
2017-08-16 19:45:58 +0100

    Add http to https middleware

--- src/server/middleware/general.js
+++ src/server/middleware/general.js
@@ -1,5 +1,7 @@
 import compression from 'compression';
+import secure from 'express-force-https';
 
 export default function setupGeneralMiddleware(app) {
   app.use(compression());
+  app.use(secure);
 }
