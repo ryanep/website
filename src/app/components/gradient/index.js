e68a9fc97d8989328566decacff6ae5420b11a3d
Ryan Elliott-Potter <ryan@ryanep.com>
2017-09-11 22:57:21 +0100

    Remove unused gradient component

--- src/app/components/gradient/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './style.scss';
-
-const Gradient = () => <div>Gradient</div>;
-
-Gradient.propTypes = {};
-
-export default Gradient;
