ff0f17e22c46c2312a48322ebe71cc735ea07040
Ryan Elliott-Potter <ryan@ryanep.com>
2017-09-11 22:27:34 +0100

    Rename header tests

---
+++ src/app/components/header/index.spec.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from './';
+import styles from './style.scss';
+
+describe('<Header />', () => {
+  it('Render component on home', () => {
+    const component = shallow(<Header isBanner={true} />);
+    expect(component.hasClass(styles.banner)).toEqual(true);
+  });
+});
