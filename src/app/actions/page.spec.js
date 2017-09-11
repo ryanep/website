c4f3b56c0083b69edbecfc3198f366b37f90a95d
Ryan Elliott-Potter <ryan@ryanep.com>
2017-09-11 22:27:17 +0100

    Add page action tests

---
+++ src/app/actions/page.spec.js
@@ -0,0 +1,44 @@
+import * as actions from './page';
+import * as types from '@constants/action-types';
+
+describe('actions/page', () => {
+  const data = {};
+  const slug = 'home';
+  const error = new Error('Error');
+
+  describe('pageFetchRequest', () => {
+    it('return PAGE_FETCH_REQUEST action type', () => {
+      expect(actions.pageFetchRequest(slug)).toMatchObject({
+        type: types.PAGE_FETCH_REQUEST
+      });
+    });
+
+    it('returns slug string', () => {
+      expect(actions.pageFetchRequest(slug)).toMatchObject({ slug });
+    });
+  });
+
+  describe('pageFetchSuccess', () => {
+    it('return PAGE_FETCH_SUCCESS action type', () => {
+      expect(actions.pageFetchSuccess(data)).toMatchObject({
+        type: types.PAGE_FETCH_SUCCESS
+      });
+    });
+
+    it('returns data object', () => {
+      expect(actions.pageFetchSuccess(data)).toMatchObject({ data });
+    });
+  });
+
+  describe('pageFetchFailure', () => {
+    it('return PAGE_FETCH_FAILURE action type', () => {
+      expect(actions.pageFetchFailure(data)).toMatchObject({
+        type: types.PAGE_FETCH_FAILURE
+      });
+    });
+
+    it('returns data object', () => {
+      expect(actions.pageFetchFailure(error)).toMatchObject({ error });
+    });
+  });
+});
