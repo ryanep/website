8495255e5f2ba5e4637841ef5e07318879babf86
Ryan Elliott-Potter <ryan@ryanep.com>
2017-10-10 19:38:53 +0100

    Add CircleCI config

--- src/server/middleware/statics.js
+++ src/server/middleware/statics.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
 export default function setupStaticMiddleware(app) {
-  app.use(express.static('dist/client/'));
+  app.use(express.static('client'));
   app.use(express.static('static'));
 }
