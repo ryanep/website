d7ae7823c7370cda83fb541c4f57be56be46aa47
Ryan Elliott-Potter <ryan@ryanep.com>
2018-04-01 22:46:18 +0100

    Temporarily disable eslint rules and fix page reducer test

--- src/app/reducers/page.spec.js
+++ src/app/reducers/page.spec.js
@@ -2,7 +2,7 @@ import reducer from '@reducers/page';
 
 describe('reducers/page', () => {
   it('should return the initial state', () => {
-    expect(reducer(null, {})).toMatchObject({
+    expect(reducer(undefined, {})).toMatchObject({
       isFetching: false,
       error: null,
       page: null
