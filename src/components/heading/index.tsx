56bb1f2265ca34153b4fa1cb94a6cf344f01974a
Ryan Elliott-Potter <ryan@ryanep.com>
2020-04-03 11:33:27 +0100

    Update heading component to modify tag as value

--- src/components/heading/index.tsx
+++ src/components/heading/index.tsx
@@ -3,5 +3,5 @@ import { HeadingProps } from './types';
 import * as styled from './styles';
 
 export const Heading: React.FC<HeadingProps> = ({ as, type, text }) => {
-  return <styled.Heading type={type} as={as}>{text}</styled.Heading>;
+  return <styled.Heading type={type} as={as ?? type}>{text}</styled.Heading>;
 };
