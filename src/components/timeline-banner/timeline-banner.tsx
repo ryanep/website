f5ea905db4b7df19c5b239cdb95824c798cfbd98
Ryan Elliott-Potter <me@ryanep.com>
2025-10-06 16:34:55 +0000

    feat: add work experience expander

--- src/components/timeline-banner/timeline-banner.tsx
+++ src/components/timeline-banner/timeline-banner.tsx
@@ -1,9 +1,12 @@
+"use client";
+import { cx } from "classix";
+import { useState } from "react";
+import { Button } from "#/components/button";
 import { Heading } from "#/components/heading";
 import { Image } from "#/components/image";
 import { Section } from "#/components/section";
 import { Spacer } from "#/components/spacer";
 import { Wrap } from "#/components/wrap";
-import { getTranslation } from "#/i18n/server";
 
 interface TimelineBannerProps {
   readonly description: string;
@@ -25,12 +28,20 @@ interface TimelineBannerProps {
   }[];
 }
 
-export const TimelineBanner = async ({
+const MAX_ITEM_COUNT = 10;
+
+export const TimelineBanner = ({
   description,
   heading,
   items,
 }: TimelineBannerProps) => {
-  const { t } = await getTranslation();
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const workItems = !isExpanded ? items.slice(0, MAX_ITEM_COUNT) : items;
+
+  const handleExpandButtonClick = () => {
+    setIsExpanded(true);
+  };
 
   return (
     <Section>
@@ -41,11 +52,16 @@ export const TimelineBanner = async ({
         <p className="mx-auto max-w-3xl text-lg font-medium">{description}</p>
         <Spacer size="large" />
 
-        <div className="relative">
+        <div
+          className={cx(
+            "relative",
+            items.length > MAX_ITEM_COUNT && !isExpanded && "mb-8"
+          )}
+        >
           <div className="absolute left-1/2 h-full w-1 -translate-x-1/2 rounded bg-neutral-300 dark:bg-neutral-400" />
 
           <div className="flex flex-col gap-4">
-            {items.map((work) => {
+            {workItems.map((work) => {
               const startDate = new Date(work.startDate);
               const endDate = work.endDate ? new Date(work.endDate) : undefined;
               const dateFormatter = new Intl.DateTimeFormat("en", {
@@ -65,12 +81,12 @@ export const TimelineBanner = async ({
                   <div className="rounded-xl bg-white p-4 shadow transition-colors dark:bg-neutral-800">
                     <header className="mb-3 flex flex-row flex-wrap items-center md:flex-nowrap">
                       <div
-                        className="mr-3 flex aspect-square size-10 items-center justify-center rounded-md"
+                        className="mr-3 flex aspect-square size-11 items-center justify-center rounded-md"
                         style={{ backgroundColor: work.colour }}
                       >
                         <Image
                           alt={work.icon.alt}
-                          className="block max-h-5 max-w-[24px] opacity-80 brightness-200"
+                          className="block max-h-6 max-w-[24px] opacity-80 brightness-200"
                           height={work.icon.height}
                           src={work.icon.url}
                           width={work.icon.width}
@@ -82,15 +98,15 @@ export const TimelineBanner = async ({
                           {work.companyName}
                         </h3>
 
-                        <p className="w-full shrink-0 text-sm font-medium md:w-auto">
-                          {formattedStartDate} -{" "}
-                          {formattedEndDate ??
-                            t("home.workExperienceBanner.present")}
+                        <p className="w-full shrink-0 text-sm font-medium md:w-auto text-neutral-400">
+                          {formattedStartDate} - {formattedEndDate ?? "Present"}
                         </p>
                       </div>
                     </header>
 
-                    <p className="text-base font-medium">{work.role}</p>
+                    <p className="text-base font-medium leading-none">
+                      {work.role}
+                    </p>
 
                     <div className="md:absolute md:top-1/2 md:size-3 md:rounded-full md:bg-neutral-400 md:group-odd:right-0 md:group-odd:translate-x-1/2 md:group-even:left-0 md:group-even:-translate-x-1/2 md:dark:bg-neutral-500" />
                   </div>
@@ -99,6 +115,14 @@ export const TimelineBanner = async ({
             })}
           </div>
         </div>
+
+        {!isExpanded ? (
+          <Button
+            onClick={handleExpandButtonClick}
+            text="Show all"
+            type="button"
+          />
+        ) : null}
       </Wrap>
     </Section>
   );
