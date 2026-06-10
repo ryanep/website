1f0d61b0315eda00ea29f5cb024ea5a9f558babb
Ryan Elliott-Potter <me@ryanep.com>
2026-06-10 17:58:33 +0000

    fix: improve icon banner accessibility with button semantics and tooltip role

--- src/components/icon-banner/icon-banner.tsx
+++ src/components/icon-banner/icon-banner.tsx
@@ -36,24 +36,29 @@ export const IconBanner = ({
 
         <ul className="flex flex-wrap justify-center gap-4">
           {items.map((item) => (
-            <li
-              className="group relative flex aspect-square w-18 shrink-0 items-center justify-center rounded-lg"
-              key={item.id}
-              style={{ backgroundColor: item.colour }}
-              // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
-              tabIndex={0}
-            >
-              <Image
-                alt={item.icon.alt}
-                className="size-auto max-w-8 max-h-8"
-                height={item.icon.height}
-                src={item.icon.url}
-                width={item.icon.width}
-              />
+            <li key={item.id}>
+              <button
+                aria-label={item.name}
+                className="group relative flex aspect-square w-18 cursor-default items-center justify-center rounded-lg border-none p-0"
+                style={{ backgroundColor: item.colour }}
+                type="button"
+              >
+                <Image
+                  alt={item.icon.alt}
+                  className="size-auto max-w-8 max-h-8"
+                  height={item.icon.height}
+                  src={item.icon.url}
+                  width={item.icon.width}
+                />
 
-              <div className="invisible absolute bottom-full left-1/2 -translate-x-1/2 rounded-sm bg-white p-1 px-2 text-sm text-black opacity-0 shadow transition-all group-hover:visible group-hover:mb-1 group-hover:opacity-100 group-focus-visible:visible group-focus-visible:mb-1 group-focus-visible:opacity-100 dark:bg-neutral-700 dark:text-white">
-                {item.name}
-              </div>
+                <div
+                  aria-hidden="true"
+                  className="invisible absolute bottom-full left-1/2 -translate-x-1/2 rounded-sm bg-white p-1 px-2 text-sm text-black opacity-0 shadow transition-all group-hover:visible group-hover:mb-1 group-hover:opacity-100 group-focus-visible:visible group-focus-visible:mb-1 group-focus-visible:opacity-100 dark:bg-neutral-700 dark:text-white"
+                  role="tooltip"
+                >
+                  {item.name}
+                </div>
+              </button>
             </li>
           ))}
         </ul>
